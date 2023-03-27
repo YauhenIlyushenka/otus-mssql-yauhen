9bcffe186952d843bbc69e53042c0068dbbfa867
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2023-03-27 17:52:25 +0200

    HW08

--- HW08-DML/homework_dml.sql
+++ HW08-DML/homework_dml.sql
@@ -122,9 +122,148 @@ GO
 /*
 4. Написать MERGE, который вставит запись в клиенты, если ее там нет, и изменит если она уже есть
 */
+GO
 
-напишите здесь свое решение
+;WITH
+	MaxNewCustomerIdCTE (MaxCustomerId) AS 
+	(
+		SELECT 
+			MAX(CustomerID)
+		FROM [Sales].[NewCustomers] as sc
+	),
+	MaxRecordFromNewAddingCustomers AS 
+	(
+		SELECT 
+			[CustomerID],
+			[CustomerName],
+			[BillToCustomerID],
+			[CustomerCategoryID],
+			[BuyingGroupID],
+			[PrimaryContactPersonID],
+			[AlternateContactPersonID],
+			[DeliveryMethodID],
+			[DeliveryCityID],
+			[PostalCityID],
+			[CreditLimit],
+			[AccountOpenedDate],
+			[StandardDiscountPercentage],
+			[IsStatementSent],
+			[IsOnCreditHold],
+			[PaymentDays],
+			[PhoneNumber],
+			[FaxNumber],
+			[DeliveryRun],
+			[RunPosition],
+			[WebsiteURL],
+			[DeliveryAddressLine1],
+			[DeliveryAddressLine2],
+			[DeliveryPostalCode],
+			[DeliveryLocation],
+			[PostalAddressLine1],
+			[PostalAddressLine2],
+			[PostalPostalCode],
+			[LastEditedBy]
+		FROM [Sales].[Customers] AS sc
+		JOIN MaxNewCustomerIdCTE AS mncCTE ON mncCTE.MaxCustomerId = SC.CustomerID
+	)
 
+MERGE [Sales].[Customers] AS target
+USING (SELECT * FROM MaxRecordFromNewAddingCustomers) AS source
+	ON (target.CustomerID = source.CustomerID)
+WHEN MATCHED 
+		THEN UPDATE SET [CustomerName] = source.CustomerName,
+						[BillToCustomerID] = source.BillToCustomerID,
+						[CustomerCategoryID] = source.CustomerCategoryID,
+						[BuyingGroupID] = source.BuyingGroupID,
+						[PrimaryContactPersonID] = source.PrimaryContactPersonID,
+						[AlternateContactPersonID] = source.AlternateContactPersonID,
+						[DeliveryMethodID] = source.DeliveryMethodID,
+						[DeliveryCityID] = source.DeliveryCityID,
+						[PostalCityID] = source.PostalCityID,
+						[CreditLimit] = source.CreditLimit,
+						[AccountOpenedDate] = source.AccountOpenedDate,
+						[StandardDiscountPercentage] = source.StandardDiscountPercentage,
+						[IsStatementSent] = source.IsStatementSent,
+						[IsOnCreditHold] = source.IsOnCreditHold,
+						[PaymentDays] = source.PaymentDays,
+						[PhoneNumber] = source.PhoneNumber,
+						[FaxNumber] = source.FaxNumber,
+						[DeliveryRun] = source.DeliveryRun,
+						[RunPosition] = source.RunPosition,
+						[WebsiteURL] = source.WebsiteURL,
+						[DeliveryAddressLine1] = source.DeliveryAddressLine1,
+						[DeliveryAddressLine2] = source.DeliveryAddressLine2,
+						[DeliveryPostalCode] = source.DeliveryPostalCode,
+						[DeliveryLocation] = source.DeliveryLocation,
+						[PostalAddressLine1] = source.PostalAddressLine1,
+						[PostalAddressLine2] = source.PostalAddressLine2,
+						[PostalPostalCode] = source.PostalPostalCode,
+						[LastEditedBy] = source.LastEditedBy
+WHEN NOT MATCHED 
+		THEN INSERT (
+			[CustomerID],
+			[CustomerName],
+			[BillToCustomerID],
+			[CustomerCategoryID],
+			[BuyingGroupID],
+			[PrimaryContactPersonID],
+			[AlternateContactPersonID],
+			[DeliveryMethodID],
+			[DeliveryCityID],
+			[PostalCityID],
+			[CreditLimit],
+			[AccountOpenedDate],
+			[StandardDiscountPercentage],
+			[IsStatementSent],
+			[IsOnCreditHold],
+			[PaymentDays],
+			[PhoneNumber],
+			[FaxNumber],
+			[DeliveryRun],
+			[RunPosition],
+			[WebsiteURL],
+			[DeliveryAddressLine1],
+			[DeliveryAddressLine2],
+			[DeliveryPostalCode],
+			[DeliveryLocation],
+			[PostalAddressLine1],
+			[PostalAddressLine2],
+			[PostalPostalCode],
+			[LastEditedBy]) 
+		VALUES (
+			source.CustomerID,
+			source.CustomerName,
+			source.BillToCustomerID,
+			source.CustomerCategoryID,
+			source.BuyingGroupID,
+			source.PrimaryContactPersonID,
+			source.AlternateContactPersonID,
+			source.DeliveryMethodID,
+			source.DeliveryCityID,
+			source.PostalCityID,
+			source.CreditLimit,
+			source.AccountOpenedDate,
+			source.StandardDiscountPercentage,
+			source.IsStatementSent,
+			source.IsOnCreditHold,
+			source.PaymentDays,
+			source.PhoneNumber,
+			source.FaxNumber,
+			source.DeliveryRun,
+			source.RunPosition,
+			source.WebsiteURL,
+			source.DeliveryAddressLine1,
+			source.DeliveryAddressLine2,
+			source.DeliveryPostalCode,
+			source.DeliveryLocation,
+			source.PostalAddressLine1,
+			source.PostalAddressLine2,
+			source.PostalPostalCode,
+			source.LastEditedBy
+			) 
+	OUTPUT deleted.*, $action, inserted.*;
+
+GO
 /*
 5. Напишите запрос, который выгрузит данные через bcp out и загрузить через bulk insert
 */
