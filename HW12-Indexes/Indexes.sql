3e1fd0baf0e8b8ac296fc7b0997ce99fa2600580
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2023-05-03 14:52:23 +0200

    HW12-Indexes

--- HW12-Indexes/Indexes.sql
+++ HW12-Indexes/Indexes.sql
@@ -1,2 +1,33 @@
 USE RentalFirm
 GO
+
+--SELECT SERVERPROPERTY('IsFullTextInstalled')
+--EXEC sp_fulltext_database 'Enable'
+--SELECT * FROM sys.fulltext_catalogs
+--SELECT name as [DBName], is_fulltext_enabled
+--FROM sys.databases
+
+--SELECT LCID, NAME
+--FROM 
+--SYS.FULLTEXT_LANGUAGES;
+
+--EXEC sp_configure 'default full-text language' --1033
+
+CREATE FULLTEXT CATALOG [RentalFirm_BrandsCatalog]
+WITH ACCENT_SENSITIVITY = ON AS DEFAULT
+GO
+
+IF NOT EXISTS(
+	SELECT * FROM [sys].[fulltext_indexes]
+	WHERE [object_id] = object_id('[Car].[Brands]'))
+BEGIN
+	CREATE FULLTEXT INDEX
+	ON [Car].[Brands]
+	(
+		[Description] LANGUAGE 1033
+	)
+	KEY INDEX [PK_Brands]
+	ON [RentalFirm_BrandsCatalog]
+	WITH STOPLIST OFF;
+END
+GO
