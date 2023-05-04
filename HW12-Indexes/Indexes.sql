d048339ac3bafacea0eaed8f5da485f8484387eb
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2023-05-04 23:17:43 +0200

    HW12

--- HW12-Indexes/Indexes.sql
+++ HW12-Indexes/Indexes.sql
@@ -15,3 +15,13 @@ PRINT N'Creating index [IX_Models_BrandID] on [Car].[Models]'
 GO
 CREATE NONCLUSTERED INDEX [IX_Models_BrandID] ON [Car].[Models] ([BrandID])
 GO
+
+PRINT N'Creating index [IX_Contracts_CarID] on [Deal].[Contracts]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Contracts_CarID] ON [Deal].[Contracts] ([CarID])
+GO
+
+PRINT N'Creating index [IX_Contracts_ClientID] on [Deal].[Contracts]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Contracts_ClientID] ON [Deal].[Contracts] ([ClientID])
+GO
