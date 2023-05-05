899fc7708a1e37928ae597a6ee3cd2191696f3a2
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2023-05-06 00:13:19 +0200

    HW12-Indexes

--- HW12-Indexes/Indexes.sql
+++ HW12-Indexes/Indexes.sql
@@ -16,21 +16,19 @@ GO
 CREATE NONCLUSTERED INDEX [IX_Models_BrandID] ON [Car].[Models] ([BrandID])
 GO
 
-PRINT N'Creating index [IX_Contracts_CarID] on [Deal].[Contracts]'
-GO
-CREATE NONCLUSTERED INDEX [IX_Contracts_CarID] ON [Deal].[Contracts] ([CarID])
-GO
-
-PRINT N'Creating index [IX_Contracts_ClientID] on [Deal].[Contracts]'
-GO
-CREATE NONCLUSTERED INDEX [IX_Contracts_ClientID] ON [Deal].[Contracts] ([ClientID])
-GO
-
-GO
+PRINT N'Creating index [[IX_Contracts_ClientID_INCLUDE_CarID]] on [Deal].[Contracts]'
 CREATE NONCLUSTERED INDEX [IX_Contracts_ClientID_CarID] ON [Deal].[Contracts] ([ClientID]) INCLUDE ([CarID])
+GO
 
 PRINT N'Creating index [IX_Contracts_CreatedDate_INCLUDE_EmployeeID] on [Deal].[Contracts]'
 GO
 CREATE NONCLUSTERED INDEX [IX_Contracts_CreatedDate_INCLUDE_EmployeeID] ON [Deal].[Contracts] ([CreatedDate]) INCLUDE ([EmployeeID])
 
---drop INDEX [IX_Employees_LastName_INCUDE_Phone] ON [Users].[Employees]
+PRINT N'Creating index [IX_Insurances_Price_INCLUDE_CarID] on [Insurance].[Insurances]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Insurances_Price_INCLUDE_CarID] on [Insurance].[Insurances] ([Price]) INCLUDE ([CarID])
+
+PRINT N'Creating index [IX_Insurances_FinishedDate_INCLUDE_CompanyID] on [Insurance].[Insurances]'
+GO
+CREATE NONCLUSTERED INDEX [IX_Insurances_FinishedDate_INCLUDE_CompanyID] on [Insurance].[Insurances] ([FinishedDate]) INCLUDE ([CompanyID])
+
