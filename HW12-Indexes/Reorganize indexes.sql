a4512b67b673aabe4216fb69ae6b2330238a2313
YauhenIlyushenka <yauhen.ilyushenka@gmail.com>
2023-06-10 15:51:12 +0200

    Small refactoring;

--- HW12-Indexes/Reorganize indexes.sql
+++ HW12-Indexes/Reorganize indexes.sql
@@ -1,3 +1,5 @@
+-- Show all indexes with percent of average fragmentation
+
 SELECT a.index_id, name, avg_fragmentation_in_percent  
 FROM sys.dm_db_index_physical_stats (DB_ID(N'RentalFirm'), NULL, NULL, NULL, NULL) AS a  
 JOIN sys.indexes AS b ON a.object_id = b.object_id AND a.index_id = b.index_id;   
@@ -7,4 +9,4 @@ GO
 ALTER INDEX IX_Models_BrandID ON [Car].[Models]
 REORGANIZE;
 GO
--- After 0
+-- After 0%
